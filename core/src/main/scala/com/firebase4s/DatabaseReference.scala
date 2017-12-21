010fbd5687958fc0218f44567b6b39c79e0f61cc
Aaron Rice <adrice727@gmail.com>
2017-12-20 17:03:57 -0800

    Implement setValue for DatabaseReference.

--- core/src/main/scala/com/firebase4s/DatabaseReference.scala
+++ core/src/main/scala/com/firebase4s/DatabaseReference.scala
@@ -1,6 +1,9 @@
 package com.firebase4s
 
-import com.google.firebase.database.{DatabaseReference => FirebaseDatabaseReference}
+import scala.concurrent.{Future, Promise}
+import com.google.firebase.database.{
+  DatabaseError => FirebaseDatabaseError,
+  DatabaseReference => FirebaseDatabaseReference}
 
 
 /**
@@ -8,4 +11,18 @@ import com.google.firebase.database.{DatabaseReference => FirebaseDatabaseRefere
   * @param path
   * @param ref
   */
-class DatabaseReference(private val path: String, private val ref: FirebaseDatabaseReference) {}
+class DatabaseReference(private val path: String, private val ref: FirebaseDatabaseReference) {
+  def setValue[A](a: A): Future[A] = {
+    val p = Promise[A]()
+    ref.setValue(a, new FirebaseDatabaseReference.CompletionListener {
+      override def onComplete(error: FirebaseDatabaseError, ref: FirebaseDatabaseReference): Unit = {
+        if (error != null) {
+          p.failure(new Exception(error.getMessage))
+        } else {
+          p.success(a)
+        }
+      }
+    })
+    p.future
+  }
+}
