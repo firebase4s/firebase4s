ff1f335555f12f7faeccebe8b9f0cdae876ad955
Aaron Rice <adrice727@gmail.com>
2018-01-12 16:04:46 -0800

    Add primary UserRecord properties.

--- core/src/main/scala/com/firebase4s/auth/UserRecord.scala
+++ core/src/main/scala/com/firebase4s/auth/UserRecord.scala
@@ -2,7 +2,14 @@ package com.firebase4s.auth
 
 import com.google.firebase.auth
 
-
-case class UserRecord(record: auth.UserRecord){
-
+case class UserRecord(user: auth.UserRecord){
+  val uid: String = user.getUid
+  val providerId: String = user.getProviderId
+  val email: Option[String] = Option(user.getEmail)
+  val emailVerified: Boolean = user.isEmailVerified
+  val phoneNumber: Option[String] = Option(user.getPhoneNumber)
+  val displayName: Option[String] = Option(user.getDisplayName)
+  val photoUrl: Option[String] = Option(user.getPhotoUrl)
+  val isDisabled: Boolean = user.isDisabled
+  val metaData: UserMetaData = UserMetaData(user.getUserMetadata)
 }
