1ee86225eea2f4fdc28af45c53d3af3929620baf
Aaron Rice <adrice727@gmail.com>
2018-01-22 13:22:09 -0800

    Ensure proper handling of options when setting ref values.

---
+++ test/src/test/scala/com/firebase4s/test/database/OptionValuesSpec.scala
@@ -0,0 +1,36 @@
+package com.firebase4s.test.database
+
+import com.firebase4s.database.{ DataSnapshot, DatabaseReference }
+import com.firebase4s.test.Test.db
+import org.scalatest._
+
+import scala.concurrent.Future
+
+class OptionValuesSpec extends AsyncWordSpecLike with Matchers {
+
+  "DatabaseReference" should {
+    "successfully set a value at its location" when {
+      "provided with an Some(value)" in {
+
+        val optionValue = Some("option")
+        val optionRef: DatabaseReference = db.ref("test/someString")
+
+        optionRef.set(optionValue)
+          .flatMap(_ => optionRef.get())
+          .map(snapshot => assert(snapshot.getValue == optionValue))
+
+      }
+    }
+
+    "successfully set null at its location" when {
+      "provided with a None" in {
+
+        val noneRef: DatabaseReference = db.ref("test/none")
+
+        noneRef.set(None)
+          .flatMap(_ => noneRef.get())
+          .map(snapshot => assert(!snapshot.exists))
+      }
+    }
+  }
+}
