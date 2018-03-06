3939342414e2710e5d48b4683c0bf35f121f1b95
Aaron Rice <adrice727@gmail.com>
2018-03-06 10:14:14 -0800

    Add basic travis config. Use env vars for test setup.

--- test/src/test/scala/com/firebase4s/test/Test.scala
+++ test/src/test/scala/com/firebase4s/test/Test.scala
@@ -1,16 +1,22 @@
 package com.firebase4s.test
 
-import java.io.InputStream
+import java.io.ByteArrayInputStream
 
 import com.firebase4s.App
 import com.firebase4s.auth.Auth
 import com.firebase4s.database.Database
+
 import scala.util.Random
 
 object Test {
-  private val serviceAccount: InputStream = getClass.getResourceAsStream("/firebase-service-account-key.json")
 
-  App.initialize(serviceAccount, "https://fir-4s.firebaseio.com")
+  private val serviceAccount =
+    Option(System.getenv("FIREBASE4S_TEST_SERVICE_ACCOUNT"))
+      .map(_.getBytes)
+      .map(new ByteArrayInputStream(_))
+      .getOrElse(getClass.getResourceAsStream("/firebase-service-account-key.json"))
+
+  App.initialize(serviceAccount, System.getenv("FIREBASE4S_TEST_DB_URL"))
 
   val randomEmail: () => String = () => s"${Random.alphanumeric.take(10).mkString}@firebase4s.com".toLowerCase
   val randomPhone: () => String = () => s"+1${(for (i <- 1 to 10) yield Random.nextInt(9) + 1).mkString}"
